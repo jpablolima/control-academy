1fd3cf941c68fdd9e7c7d02132b4e0eac0759163
jpablolima <jp.pablolima@gmail.com>
2020-10-22 12:19:26 -0300

    write the files in  data.json

--- instructors.js
+++ instructors.js
@@ -1,5 +1,7 @@
-// CREATE
+const fs = require('fs');
 
+
+// Create
 exports.post = function(req, res) {
 
     const keys = Object.keys(req.body)
@@ -9,5 +11,15 @@ exports.post = function(req, res) {
             return res.send('Please, fill all fields!')
         }
     }
+
+    // write the file
+    fs.writeFile("data.json", JSON.stringify(req.body), function(err) {
+        if (err) return res.send('Write file error!')
+        else {
+            return res.redirect('/instructors');
+        }
+    });
+
+
     return res.send(req.body)
 }
